0a2d08b08c49b94146c9ea1f21a95956624beb1b
Andrew Min <andrew@turnkey.io>
2025-09-30 17:15:50 -0400

    latest attempt: update nginx

--- export/webpack.config.js
+++ export/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.[contenthash].js',
+      publicPath: '/',
       clean: true,
     },
     module: {
