1c0f2a5490496ac232291e2591a5508330cead5c
Andrew Min <andrew@turnkey.io>
2025-10-07 01:12:31 -0400

    resolve tests

--- export-and-sign/jest.config.js
+++ export-and-sign/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   clearMocks: true,
   setupFilesAfterEnv: ["regenerator-runtime/runtime"],
   testPathIgnorePatterns: ["/node_modules/"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(@noble|@hpke)/)",
+  ],
 };
